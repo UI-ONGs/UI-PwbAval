f425d0f90540e6c943ac62ff189e73b1e3017874
Maria Luiza Arnhold <101952113+smariarnhold@users.noreply.github.com>
2024-06-04 16:41:53 -0300

    Update redes_sociais.js

--- sitezinho/js/redes_sociais.js
+++ sitezinho/js/redes_sociais.js
@@ -12,60 +12,45 @@ facebook.addEventListener("click", function() {
         let mensagem = "O usuário do Facebook inserido é: " + user_face;
         // Exibindo a mensagem concatenada
         alert(mensagem);
-       // window.location.href = "https://www.instagram.com/" + user_face + "/";
-    } else {
-        // Se o usuário clicou em "Cancelar" ou fechou a caixa de diálogo, você pode lidar com isso aqui
-        console.log("O usuário cancelou a entrada ou fechou a caixa de diálogo.");
-    }
-    
-    trocarHref1(user_face);
+        trocarHref1(user_face);
+    }     
 });
 
+// chamando a função do Facebook
 function trocarHref1(user_face) {
     var novoHref = "https://www.facebook.com/" + user_face + "/";
     document.getElementById("link1").href = novoHref;
 }
 
 
-// Adicionando um evento de clique para o botão de twitter
+// Adicionando um evento de clique para o botão de Twitter
 twitter.addEventListener("click", function() {
     let user_twitter = prompt("Por favor, insira seu usuário do Twitter:");
-    // Verificando se o usuário inseriu algum valor
     if (user_twitter !== null) {
-        // Concatenando o valor inserido pelo usuário com outro texto
-        let mensagem = "O usuário do Facebook inserido é: " + user_twitter;
-        // Exibindo a mensagem concatenada
+        let mensagem = "O usuário do Twitter inserido é: " + user_twitter;
         alert(mensagem);
-    } else {
-        // Se o usuário clicou em "Cancelar" ou fechou a caixa de diálogo, você pode lidar com isso aqui
-        console.log("O usuário cancelou a entrada ou fechou a caixa de diálogo.");
+        trocarHref2(user_twitter);
     }
     
-    trocarHref2(user_twitter);
 });
 
+// chamando a função do Twitter
 function trocarHref2(user_twitter) {
-    var novoHref = "https://www.instagram.com/" + user_twitter + "/";
+    var novoHref = "https://x.com/" + user_twitter;
     document.getElementById("link2").href = novoHref;
 }
 
-// Adicionando um evento de clique para o botão de Facebook
+// Adicionando um evento de clique para o botão de Instagram
 instagram.addEventListener("click", function() {
     let user_insta = prompt("Por favor, insira seu usuário do Instagram:");
-    // Verificando se o usuário inseriu algum valor
     if (user_insta !== null) {
-        // Concatenando o valor inserido pelo usuário com outro texto
-        let mensagem = "O usuário do Facebook inserido é: " + user_insta;
-        // Exibindo a mensagem concatenada
+        let mensagem = "O usuário do Instagram inserido é: " + user_insta;
         alert(mensagem);
-    } else {
-        // Se o usuário clicou em "Cancelar" ou fechou a caixa de diálogo, você pode lidar com isso aqui
-        console.log("O usuário cancelou a entrada ou fechou a caixa de diálogo.");
-    }
-    
-    trocarHref3(user_insta);
+        trocarHref3(user_insta);
+    } 
 });
 
+// chamando a função do Instagram
 function trocarHref3(user_insta) {
     var novoHref = "https://www.instagram.com/" + user_insta + "/";
     document.getElementById("link3").href = novoHref;
