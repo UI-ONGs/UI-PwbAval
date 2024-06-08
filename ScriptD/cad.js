79370262b2cde12e03ff941b8f66739a96d50d16
Daniel Haddad <101983469+Danhmb@users.noreply.github.com>
2024-06-08 15:10:59 -0300

    commit inicial
    commit inicial, arrumarei até o fim

---
+++ ScriptD/cad.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const nome = document.getElementById('nome').value;
+        const tipo = document.getElementById('tipo').value;
+        const desc = document.getElementById('desc').value;
+        const endereco = document.getElementById('Endereco').value;
+        const data = document.getElementById('data').value;
+        const horarioI = document.getElementById('horarioI').value;
+        const horarioF = document.getElementById('horarioF').value;
+
+        const eventData = {
+            nome,
+            tipo,
+            desc,
+            endereco,
+            data,
+            horarioI,
+            horarioF
+        };
+
+        localStorage.setItem('eventData', JSON.stringify(eventData));
+
+        window.location.href = 'detalhe.html';
+    });
+});
