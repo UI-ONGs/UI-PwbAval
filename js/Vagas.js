bf435a9b09bbb1a9779cabfd01a95be608436e41
Solidarize <165799977+UI-ONGs@users.noreply.github.com>
2024-11-05 09:56:35 -0300

    Update Vagas.js

--- js/Vagas.js
+++ js/Vagas.js
@@ -1,71 +1,63 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  var swiper = new Swiper('.conteudo', {
-    loop: true,
-
-    centeredSlides: true,
-    
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
-    },
-    
-    zoom: {
-      maxRatio: 5,
-    },
-    
-    mousewheel: {
-      invert: true,
-    },
-    
-    freeMode: {
-      enabled: true,
-      sticky: true,
-    },
-    
-    grabCursor: true,
-    
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true,
-    },
-    
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    effect: "coverflow",
-
-    coverflowEffect: {
-      rotate: -5,
-      stretch: 10,
-      depth: 100,
-      modifier: 1,
-      slideShadows: false,
-    },
-    
-    breakpoints: {
-      640: {
-        direction: 'vertical',
-        slidesPerView: 1,
-        spaceBetween: 20,
+    var swiper = new Swiper('.swiper', {
+      loop: true,
+  
+      centeredSlides: true,
+      
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
       },
-      1100: {
-        direction: 'horizontal',
-        slidesPerView: 2,
-        spaceBetween: 30,
+      
+      zoom: {
+        maxRatio: 5,
       },
-      1400: {
-        direction: 'horizontal',
-        slidesPerView: 3,
-        spaceBetween: 40,
+      
+      mousewheel: {
+        invert: true,
       },
-    },
+      
+      freeMode: {
+        enabled: true,
+        sticky: true,
+      },
+      
+      grabCursor: true,
+      
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      spaceBetween:50,
+      effect: "coverflow",
+  
+      coverflowEffect: {
+        rotate: -5,
+        stretch: 10,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+      },
+      
+      direction: 'vertical',
+      slidesPerView: 1,
+      breakpoints: {
+        768: {
+          direction: 'horizontal',
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1400: {
+          direction: 'horizontal',
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+      },
+    });
   });
-});
